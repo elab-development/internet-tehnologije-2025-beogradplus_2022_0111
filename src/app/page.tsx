722d8ded49d0393792ba22c02876806815043865
janjicmilica <janjicmilica327@gmail.com>
2026-01-11 22:14:45 +0100

    izmena mape

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,16 @@
 'use client'
 import dynamic from 'next/dynamic';
 
-function donesiMapu()
-{
-  return import ('../components/map');
+function donesiMapu() {
+  return import('../components/map');
 }
-const Map  = dynamic(donesiMapu, {ssr:false});
-   
+const Map = dynamic(donesiMapu, { ssr: false });
+
 export default function Home() {
   return (
-    <div className="d-flex align-items-center" style={{ minHeight: "100vh", backgroundColor: "#f8f9fa" }}>
-      
-      <div className="card shadow rounded overflow-hidden" 
-        style={{ height: "100vh", width: "1024px", backgroundColor: "white" }}>
+    <div style={{ height: "100vh", width: "100vw", overflow: "hidden" }}>
 
-        <div className="d-flex align-items-center mx-auto" style={{ height: "100%", width: "972px" }}>
-            <Map visina="80%" sirina="100%" /> 
-        </div>
-
-      </div>
+      <Map visina="100vh" sirina="100vw" /> 
 
     </div>
   );
