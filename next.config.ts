74547847940c2afd106822510911770c5d981e3a
damjan123456 <damjan.djuric10@gmail.com>
2026-09-14 21:38:57 +0200

    promenjen next.config zbog deploy-a

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,24 @@
 import type { NextConfig } from "next";
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
 
-const nextConfig: NextConfig = {
-  output: "standalone",
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        ...config.watchOptions,
+const nextConfig = (phase: string): NextConfig => {
+  const config: NextConfig = {
+    output: "standalone",
+  };
+
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    config.webpack = (webpackConfig) => {
+      webpackConfig.watchOptions = {
+        ...webpackConfig.watchOptions,
         poll: 1000,
         aggregateTimeout: 300,
       };
-    }
 
-    return config;
-  },
+      return webpackConfig;
+    };
+  }
+
+  return config;
 };
 
 export default nextConfig;
